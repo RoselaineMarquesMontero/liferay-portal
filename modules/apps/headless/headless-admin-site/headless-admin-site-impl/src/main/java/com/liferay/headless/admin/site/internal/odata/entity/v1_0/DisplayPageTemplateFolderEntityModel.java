1f1d57c8cd5ed820f52defa0d158ed55c99de890
Roselaine Marques <roselaine.marques@liferay.com>
2025-08-28 12:00:43 +0200

    LPD-62162 Format Source

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/odata/entity/v1_0/DisplayPageTemplateFolderEntityModel.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/odata/entity/v1_0/DisplayPageTemplateFolderEntityModel.java
@@ -1,8 +1,15 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 package com.liferay.headless.admin.site.internal.odata.entity.v1_0;
+
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.odata.entity.DateTimeEntityField;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
+
 import java.util.Map;
 
 /**
@@ -28,4 +35,5 @@ public class DisplayPageTemplateFolderEntityModel implements EntityModel {
 	}
 
 	private final Map<String, EntityField> _entityFieldsMap;
+
 }
--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/search/LayoutPageTemplateCollectionModelSearchConfigurator.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/search/LayoutPageTemplateCollectionModelSearchConfigurator.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -9,7 +9,6 @@ import com.liferay.layout.page.template.internal.search.spi.model.index.contribu
 import com.liferay.layout.page.template.model.LayoutPageTemplateCollection;
 import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionLocalService;
 import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
 import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
 import com.liferay.portal.search.spi.model.registrar.ModelSearchConfigurator;
@@ -21,9 +20,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Roselaine Marques
  */
-@Component(
-	service = ModelSearchConfigurator.class
-)
+@Component(service = ModelSearchConfigurator.class)
 public class LayoutPageTemplateCollectionModelSearchConfigurator
 	implements ModelSearchConfigurator<LayoutPageTemplateCollection> {
 
@@ -42,7 +39,7 @@ public class LayoutPageTemplateCollectionModelSearchConfigurator
 
 	@Override
 	public ModelIndexerWriterContributor<LayoutPageTemplateCollection>
-	getModelIndexerWriterContributor() {
+		getModelIndexerWriterContributor() {
 
 		return _modelIndexWriterContributor;
 	}
@@ -65,6 +62,5 @@ public class LayoutPageTemplateCollectionModelSearchConfigurator
 
 	private ModelIndexerWriterContributor<LayoutPageTemplateCollection>
 		_modelIndexWriterContributor;
-
 
 }
